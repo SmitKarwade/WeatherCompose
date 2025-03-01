cb0505f85004abe135723bab78ab1c90c8d510c6
SmitKarwade <smitkarwade31@gmail.com>
2025-03-01 18:27:37 +0530

    Added weather API dependency

---
+++ app/src/main/java/com/example/jetweather/di/AppModule.kt
@@ -0,0 +1,28 @@
+package com.example.jetweather.di
+
+import com.example.jetweather.network.WeatherAPI
+import com.example.jetweather.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideWeatherAPI(): WeatherAPI {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherAPI::class.java)
+    }
+
+}
