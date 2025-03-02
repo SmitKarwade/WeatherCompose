eb5f00f8b4e23a8f272f1f3a484f0ef3045f95c8
SmitKarwade <smitkarwade31@gmail.com>
2025-03-02 10:54:40 +0530

    Changes cause no effects

--- app/src/main/java/com/example/jetweather/main/MainActivity.kt
+++ app/src/main/java/com/example/jetweather/main/MainActivity.kt
@@ -5,14 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetweather.R
 import com.example.jetweather.navigation.AppNavigation
 import com.example.jetweather.ui.theme.JetWeatherTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,11 +37,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WeatherApp(){
+
+    val backColor = colorResource(id = R.color.back)
+    val backlineColor = colorResource(id = R.color.backline)
     JetWeatherTheme {
-        Surface(modifier = Modifier.fillMaxSize(),
-            color = Color(0xEB313541)
-        ) {
-            AppNavigation()
+        Surface(modifier = Modifier.fillMaxSize()){
+            Box(modifier = Modifier.fillMaxSize()
+                .background(brush = Brush.verticalGradient(listOf(backColor, backlineColor)))){
+
+                AppNavigation()
+            }
         }
     }
 }
