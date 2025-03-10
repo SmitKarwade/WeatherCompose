9229b6c5b66b0fbc2129ee7eb62d8e125fd7269f
SmitKarwade <smitkarwade31@gmail.com>
2025-03-10 22:38:34 +0530

    Removed unnecessary lines of code

--- app/src/main/java/com/example/jetweather/main/MainActivity.kt
+++ app/src/main/java/com/example/jetweather/main/MainActivity.kt
@@ -7,16 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.jetweather.R
 import com.example.jetweather.navigation.AppNavigation
 import com.example.jetweather.ui.theme.JetWeatherTheme
@@ -48,14 +44,5 @@ fun WeatherApp(){
                 AppNavigation()
             }
         }
-    }
-}
-
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetWeatherTheme {
     }
 }
