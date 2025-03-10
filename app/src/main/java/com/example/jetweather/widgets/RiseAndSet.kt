99c18f3ad012d758048ecbedb2fa165bbb1b8b51
SmitKarwade <smitkarwade31@gmail.com>
2025-03-10 23:04:30 +0530

    Updated progress bar

--- app/src/main/java/com/example/jetweather/widgets/RiseAndSet.kt
+++ app/src/main/java/com/example/jetweather/widgets/RiseAndSet.kt
@@ -1,19 +1,12 @@
 package com.example.jetweather.widgets
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,13 +17,28 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetweather.R
 import com.example.jetweather.util.formatDateTime
+import kotlinx.coroutines.delay
 
 @Composable
 fun sunsetSunrise(
     sunriseTime: Int,
-    sunsetTime: Int,
-    progress: Float = 0f
+    sunsetTime: Int
 ) {
+    var currentTime by remember { mutableStateOf(System.currentTimeMillis() / 1000) }
+
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(5000)
+            currentTime = System.currentTimeMillis() / 1000
+        }
+    }
+
+
+    val totalDuration = sunsetTime - sunriseTime
+    val elapsedTime = (currentTime - sunriseTime).coerceIn(0, totalDuration.toLong())
+    val progress = elapsedTime.toFloat() / totalDuration
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -49,7 +57,6 @@ fun sunsetSunrise(
                     contentDescription = "Sunrise",
                     modifier = Modifier.size(40.dp),
                     tint = Color.White
-
                 )
                 Text(text = "Sunrise", color = Color.White, fontSize = 14.sp)
             }
@@ -73,7 +80,7 @@ fun sunsetSunrise(
                 .fillMaxWidth()
                 .height(6.dp)
                 .clip(RoundedCornerShape(50)),
-            color = Color.Yellow,
+            color = Color(0xFFFF6C52),
             trackColor = Color.Gray,
         )
 
